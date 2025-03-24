25c8a61086edde8dd2c1975e29c717d34f5f570f
Much. Rifqi Hermawan <98927288+muchrifqi@users.noreply.github.com>
2025-03-24 09:15:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = 'presensi-app-v2';
 const ASSETS_TO_CACHE = [
-  '../shahaba-kepegawaian-presensi/index.html',
-  '../shahaba-kepegawaian-presensi/pages/pesertadidik.html',
-  '../shahaba-kepegawaian-presensi/pages/landing.html',
-  '../shahaba-kepegawaian-presensi/pages/landingpegawai.html',
-  '../shahaba-kepegawaian-presensi/pages/about.html',
-  '../shahaba-kepegawaian-presensi/pages/laporan.html',
-  '../shahaba-kepegawaian-presensi/assets/css/styles.css',
-  '../shahaba-kepegawaian-presensi/assets/js/script.js',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi72.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi144.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi167.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi180.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi192.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi256.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi384.png',
-  '../shahaba-kepegawaian-presensi/assets/icons/logopresensi512.png'
+  'shahaba-kepegawaian-presensi/index.html',
+  'shahaba-kepegawaian-presensi/pages/pesertadidik.html',
+  'shahaba-kepegawaian-presensi/pages/landing.html',
+  'shahaba-kepegawaian-presensi/pages/landingpegawai.html',
+  'shahaba-kepegawaian-presensi/pages/about.html',
+  'shahaba-kepegawaian-presensi/pages/laporan.html',
+  'shahaba-kepegawaian-presensi/assets/css/styles.css',
+  'shahaba-kepegawaian-presensi/assets/js/script.js',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi72.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi144.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi167.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi180.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi192.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi256.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi384.png',
+  'shahaba-kepegawaian-presensi/assets/icons/logopresensi512.png'
 ];
 
 // Install service worker
